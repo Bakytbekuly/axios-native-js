3523294e7cddac4a4228267eb66eb9c2ad021d4e
Rustem <mr.rustem071193@gmail.com>
2022-07-23 16:02:40 +0600

    post

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const axios = require("axios").default
 
-axios.get("https://fortniteapi.io/v3/challenges", {
-    params: {
-        lang: "ru",
-        season: "current",
-    },
-    headers: {
-        "Content-Type": "application/json",
-        Authorization: "608ecf29-72fa5d12-265b878f-ce68cfd1"
-    }
+// axios.get("https://fortniteapi.io/v3/challenges", {
+//     params: {
+//         lang: "ru",
+//         season: "current",
+//     },
+//     headers: {
+//         "Content-Type": "application/json",
+//         Authorization: "608ecf29-72fa5d12-265b878f-ce68cfd1"
+//     }
+// }).then(res => {
+//     console.log(res.data)
+// })
+//     .catch((err) => {
+//         console.error(err);
+//     })
+
+axios.post("https://jsonplaceholder.typicode.com/posts", {
+    userId: 1,
+    title: "My title",
+    body: "My Body",
 }).then(res => {
     console.log(res.data)
 })
