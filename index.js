e4f9de1ffaf61b0652f94038b732ba5cb1d95f63
Rustem <mr.rustem071193@gmail.com>
2022-07-23 15:56:19 +0600

    get fortnite

---
+++ index.js
@@ -0,0 +1,17 @@
+const axios = require("axios").default
+
+axios.get("https://fortniteapi.io/v3/challenges", {
+    params: {
+        lang: "ru",
+        season: "current",
+    },
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "608ecf29-72fa5d12-265b878f-ce68cfd1"
+    }
+}).then(res => {
+    console.log(res.data)
+})
+    .catch((err) => {
+        console.error(err);
+    })
